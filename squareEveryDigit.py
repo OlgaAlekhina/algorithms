28c66bb1873805a59e5842cd2cada4c18dda4d40
Olga Alekhina <alekhina.olga@gmail.com>
2024-04-25 19:37:36 +0300

    add solution for squareEveryDigit task

---
+++ squareEveryDigit.py
@@ -0,0 +1,20 @@
+def square_digits(n):
+    if n == 0:
+        return 0
+    str_n = []
+    sum = ''
+    while n != 0:
+        str_n.append(n % 10)
+        n = n // 10
+    sqr_dig = [str(i ** 2) for i in str_n][::-1]
+    print(sqr_dig)
+    for i in sqr_dig:
+        sum += i
+
+    return int(sum)
+
+
+print(square_digits(3212))
+        
+    
+    
