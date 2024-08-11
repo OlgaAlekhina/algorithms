72d9464f8aed108a46e71cd118bceb686a38eb70
OlgaAlekhina <olga-alekhina@rambler.ru>
2024-08-12 00:57:50 +0300

    add solution for firstBadVersion task

---
+++ firstBadVersion.py
@@ -0,0 +1,58 @@
+# The isBadVersion API is already defined for you.
+def is_bad_version(version: int) -> bool:
+    bad_version = 1150769282
+    if version >= bad_version:
+        return True
+    return False
+
+
+def first_bad_version(n: int) -> bool:
+    def bin_search(start, end, flag):
+        if start == end:
+            if flag is True:
+                if is_bad_version(start) is True:
+                    return start
+                return start + 1
+            else:
+                if is_bad_version(start) is False:
+                    return start + 1
+                return start
+        if start > end:
+            return start
+        mid = (end + start) // 2
+        if is_bad_version(mid) is False:
+            return bin_search(mid + 1, end, False)
+        else:
+            return bin_search(start, mid - 1, True)
+
+    return bin_search(1, n, False)
+
+
+print(first_bad_version(1420736637))
+
+# You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+#
+# Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+#
+# You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+#
+#
+#
+# Example 1:
+#
+# Input: n = 5, bad = 4
+# Output: 4
+# Explanation:
+# call isBadVersion(3) -> false
+# call isBadVersion(5) -> true
+# call isBadVersion(4) -> true
+# Then 4 is the first bad version.
+# Example 2:
+#
+# Input: n = 1, bad = 1
+# Output: 1
+#
+#
+# Constraints:
+#
+# 1 <= bad <= n <= 231 - 1
