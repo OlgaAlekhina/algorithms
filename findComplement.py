83328fb3b3f331194e3b73671c2a14c65660838d
Olga Alekhina <alekhina.olga@gmail.com>
2024-08-22 14:37:40 +0300

    add solution for findComplement task

---
+++ findComplement.py
@@ -0,0 +1,33 @@
+def find_compliment(num: int) -> int:
+    compliment, exp = 0, 0
+    while num:
+        compliment += 2 ** exp if not num & 1 else 0
+        exp += 1
+        num >>= 1
+
+    return compliment
+
+print(find_compliment(1))
+
+# The complement of an integer is the integer you get when you flip all the 0's to 1's and all the 1's to 0's in its binary representation.
+#
+# For example, The integer 5 is "101" in binary and its complement is "010" which is the integer 2.
+# Given an integer num, return its complement.
+#
+#
+#
+# Example 1:
+#
+# Input: num = 5
+# Output: 2
+# Explanation: The binary representation of 5 is 101 (no leading zero bits), and its complement is 010. So you need to output 2.
+# Example 2:
+#
+# Input: num = 1
+# Output: 0
+# Explanation: The binary representation of 1 is 1 (no leading zero bits), and its complement is 0. So you need to output 0.
+#
+#
+# Constraints:
+#
+# 1 <= num < 231
