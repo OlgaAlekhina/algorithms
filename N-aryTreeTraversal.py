3161e6a7d7d36afedb798245e1ed799411b1bb71
Olga Alekhina <alekhina.olga@gmail.com>
2024-08-27 12:37:58 +0300

    add solutions for N-aryTreeTraversal task

---
+++ N-aryTreeTraversal.py
@@ -0,0 +1,59 @@
+from typing import List
+
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        if not root:
+            return []
+        stack, res, visited = [root], [], []
+        while stack:
+            if stack[-1] not in visited and stack[-1].children:
+                visited.append(stack[-1])
+                for node in stack[-1].children[::-1]:
+                    stack.append(node)
+            else:
+                res.append(stack[-1].val)
+                stack.pop()                
+
+        return res
+
+
+n5 = Node(5)
+n6 = Node(6)
+n3 = Node(3, [n5, n6])
+n2 = Node(2)
+n4 = Node(4)
+n1 = Node(1, [n3, n2, n4])
+
+print(Solution().postorder(n1))
+
+# Given the root of an n-ary tree, return the postorder traversal of its nodes' values.
+#
+# Nary-Tree input serialization is represented in their level order traversal. Each group of children is separated by the null value (See examples)
+#
+#
+#
+# Example 1:
+#
+#
+# Input: root = [1,null,3,2,4,null,5,6]
+# Output: [5,6,3,2,4,1]
+# Example 2:
+#
+#
+# Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+# Output: [2,6,14,11,7,3,12,8,4,13,9,10,5,1]
+#
+#
+# Constraints:
+#
+# The number of nodes in the tree is in the range [0, 104].
+# 0 <= Node.val <= 104
+# The height of the n-ary tree is less than or equal to 1000.
