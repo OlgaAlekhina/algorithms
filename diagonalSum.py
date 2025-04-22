c36939bb9ddad3500935f3fe32587d4208940489
Olga Alekhina <alekhina.olga@gmail.com>
2025-04-22 16:21:46 +0300

    add solution for diagonalSum task

---
+++ diagonalSum.py
@@ -0,0 +1,54 @@
+from typing import List
+
+
+def diagonal_sum(mat: List[List[int]]) -> int:
+    i, j, res = 0, len(mat) - 1, 0
+    for row in mat:
+        if i == j:
+            res += row[i]
+        else:
+            res += row[i] + row[j]
+        i += 1
+        j -= 1
+
+
+    return res
+
+
+print(diagonal_sum([[1,1,1,1,2,3],[7,1,6,1,1,1],[1,8,9,1,1,1],[1,12,10,1,1,1], [3,4,8,12,45,9],[1,2,3,4,5,6]]))
+
+# https://leetcode.com/problems/matrix-diagonal-sum/description/
+#
+# Given a square matrix mat, return the sum of the matrix diagonals.
+#
+# Only include the sum of all the elements on the primary diagonal and all the elements on the secondary diagonal that are not part of the primary diagonal.
+#
+#
+#
+# Example 1:
+#
+#
+# Input: mat = [[1,2,3],
+#               [4,5,6],
+#               [7,8,9]]
+# Output: 25
+# Explanation: Diagonals sum: 1 + 5 + 9 + 3 + 7 = 25
+# Notice that element mat[1][1] = 5 is counted only once.
+# Example 2:
+#
+# Input: mat = [[1,1,1,1],
+#               [1,1,1,1],
+#               [1,1,1,1],
+#               [1,1,1,1]]
+# Output: 8
+# Example 3:
+#
+# Input: mat = [[5]]
+# Output: 5
+#
+#
+# Constraints:
+#
+# n == mat.length == mat[i].length
+# 1 <= n <= 100
+# 1 <= mat[i][j] <= 100
